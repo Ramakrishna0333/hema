b0121a0171af9a8b531b9556900a525a174c6892
Ramakrishna0333 <92657155+Ramakrishna0333@users.noreply.github.com>
2021-10-17 16:39:27 +0530

    Add files via upload

---
+++ hema.py.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+string=input("Enter the string: ")
+newstr=list(string)
+newlist=[]
+for j in newstr:
+    if j not in newlist:
+        newlist.append(j)
+        count=0
+        for i in range(len(newstr)):
+            if j==newstr[i]:
+                count+=1
+        print("{},{}".format(j,count))
+
+
+# In[ ]:
+
+
+
+
